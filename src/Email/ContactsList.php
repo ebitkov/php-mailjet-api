2a8ee0243bca7287dc139f6fac4e58767310a972
Emanuel Bitkov <emanuel.bitkov@regenta-verlag.de>
2024-01-17 00:48:12 +0100

    define ContactsList

--- src/Email/ContactsList.php
+++ src/Email/ContactsList.php
@@ -2,6 +2,22 @@
 
 namespace ebitkov\Mailjet\Email;
 
+use DateTimeImmutable;
+
+/**
+ * Contact List objects help you organize your contacts into lists.
+ *
+ * @see https://dev.mailjet.com/email/reference/contacts/contact-list/
+ */
 final class ContactsList implements Resource
 {
+    public bool $isDeleted;
+    public string $name;
+    public string $address;
+    public DateTimeImmutable $createdAt;
+    public int $id;
+    public int $subscriberCount;
+
+    # todo: getSubscriber()
+    # todo: getSubscribedContacts()
 }
