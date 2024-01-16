029bc95cce9323c3d4c8c09a776a55270094f7c5
Emanuel Bitkov <emanuel.bitkov@regenta-verlag.de>
2024-01-17 00:01:29 +0100

    docs

--- src/Email/Resource.php
+++ src/Email/Resource.php
@@ -2,6 +2,12 @@
 
 namespace ebitkov\Mailjet\Email;
 
+/**
+ * This interface is mainly used for type declaration.
+ * All data objects MUST implement this interface.
+ *
+ * @internal
+ */
 interface Resource
 {
 }
