b4988d6f08d46018c31a642ac5510d667ba442e0
Emanuel Bitkov <emanuel.bitkov@regenta-verlag.de>
2024-01-23 13:29:08 +0100

    adding helper methods to get subscriptions and contacts

--- src/Email/v3/ContactsList.php
+++ src/Email/v3/ContactsList.php
@@ -3,7 +3,11 @@
 namespace ebitkov\Mailjet\Email\v3;
 
 use DateTimeImmutable;
+use ebitkov\Mailjet\ClientAware;
 use ebitkov\Mailjet\Email\Resource;
+use ebitkov\Mailjet\RequestAborted;
+use ebitkov\Mailjet\RequestFailed;
+use ebitkov\Mailjet\Result;
 
 /**
  * Contact List objects help you organize your contacts into lists.
@@ -12,6 +16,9 @@ use ebitkov\Mailjet\Email\Resource;
  */
 final class ContactsList implements Resource
 {
+    use ClientAware;
+
+
     public bool $isDeleted;
     public string $name;
     public string $address;
@@ -19,6 +26,33 @@ final class ContactsList implements Resource
     public int $id;
     public int $subscriberCount;
 
-    # todo: getSubscriber()
-    # todo: getSubscribedContacts()
+    /**
+     * Returns all subscriptions of this list.
+     *
+     * @return Result<Subscription>
+     *
+     * @throws RequestAborted
+     * @throws RequestFailed
+     */
+    public function getSubscriptions(): Result
+    {
+        return $this->client->getListRecipients([
+            'ContactsList' => $this->id
+        ]);
+    }
+
+    /**
+     * Gets all subscribed contacts to this list.
+     *
+     * @return Result<Contact>
+     *
+     * @throws RequestAborted
+     * @throws RequestFailed
+     */
+    public function getSubscribedContacts(): Result
+    {
+        return $this->client->getContacts([
+            'ContactsList' => $this->id
+        ]);
+    }
 }
---
+++ tests/Email/v3/ContactsListTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ebitkov\Mailjet\Tests\Email\v3;
+
+use ebitkov\Mailjet\Email\v3\Contact;
+use ebitkov\Mailjet\Email\v3\ContactsList;
+use ebitkov\Mailjet\Email\v3\Subscription;
+use ebitkov\Mailjet\Tests\MailjetApiTestCase;
+
+class ContactsListTest extends MailjetApiTestCase
+{
+    public function testGetSubscriptions(): void
+    {
+        $list = $this->getContactsList();
+        $subscriptions = $list->getSubscriptions();
+
+        $this->assertSame(1, $subscriptions->count());
+        $this->assertInstanceOf(Subscription::class, $subscriptions->first());
+        $this->assertSame(1, $subscriptions->first()->getListId());
+    }
+
+    private function getContactsList(): ContactsList
+    {
+        $client = $this->getClient();
+        return $client->getContactsListById(1);
+    }
+
+    public function testGetSubscribedContacts(): void
+    {
+        $list = $this->getContactsList();
+        $contacts = $list->getSubscribedContacts();
+
+        $this->assertSame(1, $contacts->count());
+        $this->assertInstanceOf(Contact::class, $contacts->first());
+    }
+}
